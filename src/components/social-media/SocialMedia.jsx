916c8b8615dab357cd72fea426723186cf4d4047
Carlos Javier Mathias Carcamo <carlosmts@gmail.com>
2024-02-28 01:48:07 -0300

    Increased social media icons size.

--- src/components/social-media/SocialMedia.jsx
+++ src/components/social-media/SocialMedia.jsx
@@ -5,10 +5,10 @@ import { ReactComponent as LinkedinLogo } from 'assets/logo-linkedin.svg';
 const SocialMedia = () => {
   return (
     <div className="social-media">
-      <a href="https://github.com/carlosmths">
+      <a href="https://github.com/carlosmths" target="_blank" rel="noreferrer">
         <GithubLogo />
       </a>
-      <a href="https://www.linkedin.com/in/carlos-javier-mathias-carcamo-91322036/">
+      <a href="https://www.linkedin.com/in/carlos-javier-mathias-carcamo-91322036/" target="_blank" rel="noreferrer">
         <LinkedinLogo />  
       </a>
     </div>

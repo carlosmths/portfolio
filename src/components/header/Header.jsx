72ae2777f404fe695fea7d962375262e08c02b93
Carlos Javier Mathias Carcamo <carlosmts@gmail.com>
2024-02-28 00:50:44 -0300

    Added media queries and burger icon in the header.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,21 @@
 import 'components/header/Header.scss';
+import { ReactComponent as BurgerIcon } from 'assets/icon-burger.svg';
+import { useState } from 'react';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuOnClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
     <header className="header">
       <div className="header-logo">carlosmths.dev</div>
-      <nav className="header-navigation">
+      <div className={`burger-menu ${isMenuOpen ? 'menu-open' : ''}`} onClick={menuOnClick}>
+        <BurgerIcon />
+      </div>
+      <nav className={`header-navigation ${isMenuOpen ? 'menu-open' : ''}`}>
         <ul>
           <li><a href="#home" className="active">Home</a></li>
           <li><a href="#about">About</a></li>
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,13 +6,15 @@ import TechStack from 'components/tech-stack/TechStack';
 const Hero = () => {
   return (
     <div className="hero-main">
-      <div className="column-info">
-        <h1 className="title">Front-end web developer</h1>
-        <p className="text">Hi, I'm Carlos Mathias, an enthusiastic front-end web developer based in Mar del Plata, Buenos Aires, Argentina.</p>
-        <SocialMedia />
-      </div>
-      <div className="column-image">
-        <img src={ProfileImage} alt="Profile"></img>
+      <div className="columns-group">
+        <div className="column-info">
+          <h1 className="title">Front-end web developer</h1>
+          <p className="text">Hi, I'm Carlos Mathias, an enthusiastic front-end web developer based in Mar del Plata, Buenos Aires, Argentina.</p>
+          <SocialMedia />
+        </div>
+        <div className="column-image">
+          <img src={ProfileImage} alt="Profile"></img>
+        </div>
       </div>
       <TechStack />
     </div>

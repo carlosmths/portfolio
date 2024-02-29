dd97ad85d2294db9b000a7f766e0595b18755341
Carlos Javier Mathias Carcamo <carlosmths@gmail.com>
2024-02-29 20:13:04 -0300

    Recaptcha working on submit.

--- src/components/contact-form/ContactForm.jsx
+++ src/components/contact-form/ContactForm.jsx
@@ -1,25 +1,23 @@
 import 'components/contact-form/ContactForm.scss';
-import { useRef } from 'react';
+import { useCallback } from 'react';
+import { useGoogleReCaptcha } from 'react-google-recaptcha-v3';
 
 const ContactForm = () => {
-  const recaptchaRef = useRef(null);
+  const { executeRecaptcha } = useGoogleReCaptcha();
 
-  const handleRecaptchaChange = (value) => {
-    // Handle reCAPTCHA response here
-    console.log('reCAPTCHA value:', value);
-  };
-
-  const submitHandler = (event) => {
+  const handleReCaptchaVerify = useCallback(async (event) => {
     event.preventDefault();
-    console.log('LIL PAP')
-    const recaptchaValue = recaptchaRef.current.getValue();
-    if(recaptchaValue) {
-      console.log('SEND IT', recaptchaValue);
+    if (!executeRecaptcha) {
+      console.log('Execute recaptcha not yet available');
+      return;
     }
-  }
+
+    const token = await executeRecaptcha('contactForm');
+    console.log('TOKEN', token);
+  }, [executeRecaptcha]);
 
   return (
-    <form className="contact-form" onSubmit={() => submitHandler()}>
+    <form className="contact-form" onSubmit={handleReCaptchaVerify}>
       <div className="form-group">
         <div className="form-control">
           <label htmlFor="inputName">Name</label>
@@ -34,6 +32,10 @@ const ContactForm = () => {
         <label htmlFor="inputMessage">Message</label>
         <textarea id="inputMessage" required></textarea>
       </div>
+      <small>This site is protected by reCAPTCHA and the Google&nbsp;
+        <a href="https://policies.google.com/privacy">Privacy Policy</a> and&nbsp;
+        <a href="https://policies.google.com/terms">Terms of Service</a> apply.
+      </small>
       <button type="submit" className="btn">SEND MESSAGE</button>
     </form>
   )

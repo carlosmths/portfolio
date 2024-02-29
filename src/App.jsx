964110d6f69746fb39c2302a15695a939e74ba4f
Carlos Javier Mathias Carcamo <carlosmts@gmail.com>
2024-02-29 04:26:15 -0300

    Included recaptcha v3.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import LandingPage from 'pages/LandingPage';
 import 'globals/styles/main.scss';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
 function App() {
   return (
-    <LandingPage />
+    <GoogleReCaptchaProvider reCaptchaKey={process.env.REACT_APP_SITE_KEY} useEnterprise={true}>
+      <LandingPage />
+    </GoogleReCaptchaProvider>
   );
 }
 
--- src/components/contact-form/ContactForm.jsx
+++ src/components/contact-form/ContactForm.jsx
@@ -1,8 +1,25 @@
 import 'components/contact-form/ContactForm.scss';
+import { useRef } from 'react';
 
 const ContactForm = () => {
+  const recaptchaRef = useRef(null);
+
+  const handleRecaptchaChange = (value) => {
+    // Handle reCAPTCHA response here
+    console.log('reCAPTCHA value:', value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log('LIL PAP')
+    const recaptchaValue = recaptchaRef.current.getValue();
+    if(recaptchaValue) {
+      console.log('SEND IT', recaptchaValue);
+    }
+  }
+
   return (
-    <form className="contact-form">
+    <form className="contact-form" onSubmit={() => submitHandler()}>
       <div className="form-group">
         <div className="form-control">
           <label htmlFor="inputName">Name</label>

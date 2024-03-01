eca37a81c86384e0e94fa60a829f6ad71d90ef69
Carlos Javier Mathias Carcamo <carlosmths@gmail.com>
2024-03-01 19:49:52 -0300

    Changed fonts, font-size, and adjusted responsiveness.

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <div className="hero-main">
       <div className="columns-group">
         <div className="column-info">
-          <h1 className="title">Front-end web developer</h1>
+          <h1 className="title">FRONT-END WEB DEVELOPER</h1>
           <p className="text">Hi, I'm Carlos Mathias, an enthusiastic front-end web developer based in Mar del Plata, Buenos Aires, Argentina.</p>
           <SocialMedia />
         </div>

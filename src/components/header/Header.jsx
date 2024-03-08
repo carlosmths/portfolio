e6e9be530312609a9314611b2e7e6c11df06fdad
Carlos Javier Mathias Carcamo <carlosmts@gmail.com>
2024-03-08 05:42:47 -0300

    Added logo and adjusted responsiveness in some components.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import 'components/header/Header.scss';
 import { useState } from 'react';
 import BurgerMenu from 'components/burger-menu/BurgerMenu';
+import { ReactComponent as Logo } from 'assets/logo.svg'
+import Container from 'components/container/Container';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -15,23 +17,25 @@ const Header = () => {
     const section = document.querySelector(sectionId);
     const header = document.querySelector('.header');
     window.scrollTo({
-        top: section.offsetTop - header.clientHeight,
-        behavior: 'smooth'
+      top: section.offsetTop - header.clientHeight,
+      behavior: 'smooth'
     });
-}
+  }
 
   return (
     <header className="header">
-      <div className="header-logo">carlosmthsdev</div>
-      <BurgerMenu isMenuOpen={isMenuOpen} onClick={menuOnClick} />
-      <nav className={`header-navigation ${isMenuOpen ? 'menu-open' : ''}`}>
-        <ul>
-          <li><a href="#home" onClick={scrollToSection}>Home</a></li>
-          <li><a href="#about" onClick={scrollToSection}>About</a></li>
-          <li><a href="#projects" onClick={scrollToSection}>Projects</a></li>
-          <li><a href="#contact" onClick={scrollToSection}>Contact</a></li>
-        </ul>
-      </nav>
+      <Container>
+        <Logo className="header-logo" />
+        <BurgerMenu isMenuOpen={isMenuOpen} onClick={menuOnClick} />
+        <nav className={`header-navigation ${isMenuOpen ? 'menu-open' : ''}`}>
+          <ul>
+            <li><a href="#home" onClick={scrollToSection}>Home</a></li>
+            <li><a href="#about" onClick={scrollToSection}>About</a></li>
+            <li><a href="#projects" onClick={scrollToSection}>Projects</a></li>
+            <li><a href="#contact" onClick={scrollToSection}>Contact</a></li>
+          </ul>
+        </nav>
+      </Container>
     </header>
   );
 }
